589fd9f3da0b45051fce2a0163079be0d0c7c5b9
Michael Holthausen <mholthausen@terrestris.de>
2026-07-28 15:40:49 +0200

    chore: add GitHub Actions workflows for deployment and testing

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -12,14 +12,13 @@ const config: Config = {
     v4: true, // Improve compatibility with the upcoming Docusaurus v4
   },
 
-  // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  url: 'https://gdi-be.github.io',
+  baseUrl: '/mde-docs/',
 
   organizationName: 'gdi-be',
   projectName: 'mde-docs',
+  deploymentBranch: 'gh-pages',
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
 
